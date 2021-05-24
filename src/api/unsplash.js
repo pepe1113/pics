d8f4d619cf3a824025c558f6f3db98427bbf7233
pepe1113 <venus841113@gmail.com>
2021-05-24 22:04:26 +0800

    Updates

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com/',
-  headers: {
-    Authorization: 'Client-ID sgtXkX0jNOyUC5QIqc7TWH7yfsLata1txF7bXQLIKCU',
-  },
-})
